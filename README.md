003199655c885c45d50f3064602254ed6357bbfb
Abilkhair Seilov <abilkhairseilov@outlook.com>
2025-10-27 10:32:15 +0300

    [feat] example

---
+++ README.md
@@ -0,0 +1,12 @@
+# JusticeCompanion
+
+## Requirements
+python>=3.14
+
+## Installation
+
+Make a virtual environment:
+```py
+python -m venv .venv/
+```
+
--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
+class AIHelper:
+    def fib(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 0:
+            return 0
+        res: int = AIHelper.fib(self, n-1) + AIHelper.fib(self, n-2)
+        return res
+
 if __name__ == "__main__":
     print("Hello World!")
+
+AI = AIHelper()
+print(AI.fib(5))
