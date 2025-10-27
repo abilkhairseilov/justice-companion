0bf0b2f81c2047a19b71b3139d1c29ea1119671d
Abilkhair Seilov <abilkhairseilov@outlook.com>
2025-10-27 16:29:06 +0300

    [feat] finish training
    1000 epochs, learning rate at 0.01
    loss asymptote at 32
    to predict with new data should be as simple as forward propagation (calculate z1, a1 and then y_pred)

--- src/train.py
+++ src/train.py
@@ -6,17 +6,14 @@ import seaborn as sns
 from scipy import stats
 
 dataset = pd.read_csv("data/dataset_preprocessed.csv")
-gdp_pcap = dataset['gdp_pcap']
-co2_pcap = dataset['co2_pcap']
-child_mort_pcap = dataset['child_mort_pcap']
+gdp_pcap = dataset["gdp_pcap"]
+co2_pcap = dataset["co2_pcap"]
+child_mort_pcap = dataset["child_mort_pcap"]
 
-X = dataset[['gdp_pcap', 'co2_pcap', 'child_mort_pcap']].values
-y = dataset['lex'].values
+X = dataset[["gdp_pcap", "co2_pcap", "child_mort_pcap"]].values
+y = dataset["lex"].values
 
 # start weight and bias initializationw
-
-lr = 0.01
-
 # 3 inputs, 4 hidden neurons
 w1 = np.random.randn(3, 4) * 0.01
 b1 = np.zeros(4)
@@ -25,31 +22,40 @@ b1 = np.zeros(4)
 w2 = np.random.randn(4, 1) * 0.01
 b2 = np.zeros(1)
 
-# forward propagation
-z1 = X @ w1 + b1
-a1 = np.maximum(0, z1)
+lr = 0.01
+epochs = 1000
+for epoch in range(epochs):
+    # forward propagation
+    z1 = X @ w1 + b1
+    a1 = np.maximum(0, z1)
 
-y_pred = a1 @ w2 + b2
+    y_pred = a1 @ w2 + b2
 
-# backwards propagation
-# derivative of loss with respect to predicted y
-# loss = np.mean((y_pred.squeeze() - y) ** 2)
+    # backwards propagation
+    # derivative of loss with respect to predicted y
+    loss = np.mean((y_pred.squeeze() - y) ** 2)
 
-dL_dy_pred = 2 * (y_pred.squeeze() - y) / len(y)  # pyright: ignore[reportUnknownVariableType, reportOperatorIssue]
+    dL_dy_pred = 2 * (y_pred.squeeze() - y) / len(y)  # pyright: ignore[reportUnknownVariableType, reportOperatorIssue]
 
-dL_dw2 = a1.T @ dL_dy_pred.reshape(-1,1)
-dL_db2 = np.sum(dL_dy_pred)
+    dL_dw2 = a1.T @ dL_dy_pred.reshape(-1, 1)
+    dL_db2 = np.sum(dL_dy_pred)
 
-da1 = dL_dy_pred.reshape(-1,1) @ w2.T
-dz1 = da1 * (z1 > 0).astype(float)
-dL_dw1 = X.T @ dz1
-dL_db1 = np.sum(dz1, axis=0)
+    da1 = dL_dy_pred.reshape(-1, 1) @ w2.T
+    dz1 = da1 * (z1 > 0).astype(float)
+    dL_dw1 = X.T @ dz1
+    dL_db1 = np.sum(dz1, axis=0)
 
-w1 -= lr * dL_dw1
-b1 -= lr * dL_db1
-w2 -= lr * dL_dw2
-b2 -= lr * dL_db2
+    w1 -= lr * dL_dw1
+    b1 -= lr * dL_db1
+    w2 -= lr * dL_dw2
+    b2 -= lr * dL_db2
 
+    if epoch % 100 == 0:
+        print(f"epoch: {epoch}\nloss: {loss}")
+
+
+# training done
 # print(loss)
 # print("PREDICTED Y")
-# print(y_pred)
+print(f"epoch: {epoch}\nloss: {loss}")
+print(y_pred)
