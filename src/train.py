114f4d4d933902082c964f3503ad197f0730a28e
Abilkhair Seilov <abilkhairseilov@outlook.com>
2025-10-28 09:34:07 +0300

    [feat] split data into training and testing
    dataset_processed.csv was split into 80/20 - 80% training and 20% testing

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 
@@ -5,6 +6,10 @@ dataset = pd.read_csv("data/dataset_preprocessed.csv")
 
 X = dataset[["gdp_pcap", "co2_pcap", "child_mort_pcap"]].values
 y = dataset["lex"].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # start weight and bias initializationw
 # 3 inputs, 4 hidden neurons
@@ -15,27 +20,27 @@ b1 = np.zeros(4)
 w2 = np.random.randn(4, 1) * 0.01
 b2 = np.zeros(1)
 
-lr = 0.01
-epochs = 1000
+lr = 0.001
+epochs = 5000
 for epoch in range(epochs):
     # forward propagation
-    z1 = X @ w1 + b1
+    z1 = X_train @ w1 + b1
     a1 = np.maximum(0, z1)
 
     y_pred = a1 @ w2 + b2
 
     # backwards propagation
     # derivative of loss with respect to predicted y
-    loss = np.mean((y_pred.squeeze() - y) ** 2)
+    loss = np.mean((y_pred.squeeze() - y_train) ** 2)
 
-    dL_dy_pred = 2 * (y_pred.squeeze() - y) / len(y)  # pyright: ignore[reportUnknownVariableType, reportOperatorIssue]
+    dL_dy_pred = 2 * (y_pred.squeeze() - y_train) / len(y_train)  # pyright: ignore[reportUnknownVariableType, reportOperatorIssue]
 
     dL_dw2 = a1.T @ dL_dy_pred.reshape(-1, 1)
     dL_db2 = np.sum(dL_dy_pred)
 
     da1 = dL_dy_pred.reshape(-1, 1) @ w2.T
     dz1 = da1 * (z1 > 0).astype(float)
-    dL_dw1 = X.T @ dz1
+    dL_dw1 = X_train.T @ dz1
     dL_db1 = np.sum(dz1, axis=0)
 
     w1 -= lr * dL_dw1
@@ -59,11 +64,17 @@ for epoch in range(epochs):
 # qatar has:
 # 0.15619315758173222, 0.15925531524253664, 0.04098349977481082, 77.31
 
-qatar = np.array([0.15619315758173222, 0.15925531524253664, 0.04098349977481082])
+# qatar = np.array([0.15619315758173222, 0.15925531524253664, 0.04098349977481082])
+#
+# z1 = qatar @ w1 + b1
+# a1 = np.maximum(0, z1)
+# y_pred = a1 @ w2 + b2
+#
+#
+# print(f"y_pred: {y_pred}, actual lex: 77.31")
 
-z1 = qatar @ w1 + b1
+z1 = X_test @ w1 + b1
 a1 = np.maximum(0, z1)
-y_pred = a1 @ w2 + b2
+y_pred_test = a1 @ w2 + b2
 
-
-print(f"y_pred: {y_pred}, actual lex: 77.31")
+print(y_pred_test[25], y_test[25])
